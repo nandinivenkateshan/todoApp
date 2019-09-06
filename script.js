8d72ae0d3d15e780ba465b250341f24896e013db
nandinivenkateshan <nandinivenkatesan809@gmail.com>
2019-09-06 13:53:42 +0530

    note working

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 class View {
   constructor () {
     this.app = document.querySelector('#root')
@@ -19,16 +18,13 @@ class View {
     this.submitBtn.classList.add('submit')
     // list
     this.ul = document.createElement('ul')
-    // Appending Input box and Submit button inside the form
     this.form.append(this.input, this.submitBtn)
-    // Appending header and form
     this.app.append(this.header, this.form, this.ul)
-
     this.textAreaInput = ''
   }
 
   displayTodos (data) {
-    // console.log("initial")
+    console.log('initial')
     // To remove the previus input lists
     while (this.ul.firstChild) {
       this.ul.removeChild(this.ul.firstChild)
@@ -39,14 +35,15 @@ class View {
       this.para.textContent = 'Nothing is Added to todo'
       this.ul.append(this.para)
     } else {
-      // console.log('from view.displayTodos')
-      // console.log(data)
+      console.log('from view.displayTodos')
+      console.log(data)
       data.forEach(todoData => {
         this.li = document.createElement('li')
-        // console.log('display')
-        // console.log(typeof  todoData.id)
+        console.log('display')
+        console.log(typeof todoData.id)
         this.li.id = todoData.id
-        // console.log(this.li.id)
+        console.log(this.li.id)
+
         // checkbox
         this.checkBox = document.createElement('input')
         this.checkBox.type = 'checkbox'
@@ -69,103 +66,112 @@ class View {
         this.noteBtn.classList.add('noteBtn')
         this.addNote()
 
-        // Display Note
-        // if (todoData.note) {
-        //   this.popUpBox = document.createElement('div')
-        //   this.popUpBox.id.add('popUp')
-        // }
         // Delete buttton
         this.deleteBtn = document.createElement('button')
         this.deleteBtn.textContent = 'Delete'
         this.deleteBtn.classList.add('delete')
         this.deleteItem()
-        // Append to li
-        this.li.append(this.checkBox, this.textArea, this.deleteBtn, this.noteBtn)
+
+        this.li.append(
+          this.checkBox,
+          this.textArea,
+          this.deleteBtn,
+          this.noteBtn
+        )
         this.ul.append(this.li)
-        // console.log(data)
+        console.log(data)
       })
     }
   }
 
-  addItem (data) {
-    console.log(data)
+  addItem () {
     this.form.addEventListener('click', event => {
-      data = JSON.parse(localStorage.getItem('todos')) || []
+      let data = JSON.parse(localStorage.getItem('todos')) || []
       if (event.target.className === 'submit') {
         event.preventDefault()
         if (this.input.value) {
           const todoData = {
-            id: (data.length > 0) ? data[data.length - 1].id + 1 : 1,
+            id: data.length > 0 ? data[data.length - 1].id + 1 : 1,
             text: this.input.value,
             complete: false,
             note: false,
-            noteText: 'FirstName'
+            noteText: ''
           }
           data.push(todoData)
         }
-        // console.log('from view.additem')
-        // console.log(data)
+        console.log('from view.additem')
+        console.log(data)
         this.displayTodos(data)
         localStorage.setItem('todos', JSON.stringify(data))
-        // console.log(data)
       }
       this.input.value = ''
     })
-    // this.displayTodos(data)
   }
 
-  deleteItem (data) {
+  deleteItem () {
     this.deleteBtn.addEventListener('click', event => {
-      data = JSON.parse(localStorage.getItem('todos')) || []
+      let data = JSON.parse(localStorage.getItem('todos')) || []
       if (event.target.className === 'delete') {
         let parentId = parseInt(event.target.parentElement.id)
         data = data.filter(items => items.id !== parentId)
       }
-      // console.log('from view.deleteItem')
-      // console.log(data)
+      console.log('from view.deleteItem')
+      console.log(data)
       this.displayTodos(data)
       localStorage.setItem('todos', JSON.stringify(data))
     })
   }
 
-  checkBoxClick (data) {
+  checkBoxClick () {
     this.ul.addEventListener('click', event => {
-      data = JSON.parse(localStorage.getItem('todos')) || []
+      let data = JSON.parse(localStorage.getItem('todos')) || []
       if (event.target.type === 'checkbox') {
-        // console.log(event.target.parentElement)
+        console.log(event.target.parentElement)
         let parentId = parseInt(event.target.parentElement.id)
-        // console.log(typeof data[0].id)
-        // console.log(typeof parentId)
+        console.log(typeof data[0].id)
+        console.log(typeof parentId)
 
-        data = data.map(todo => (parentId === todo.id) ? { id: todo.id,
-          text: todo.text,
-          complete: !todo.complete,
-          note: todo.note,
-          noteText: todo.noteText } : todo)
-        // console.log(data)
+        data = data.map(todo =>
+          parentId === todo.id
+            ? {
+              id: todo.id,
+              text: todo.text,
+              complete: !todo.complete,
+              note: todo.note,
+              noteText: todo.noteText
+            }
+            : todo
+        )
+        console.log(data)
         this.displayTodos(data)
         localStorage.setItem('todos', JSON.stringify(data))
       }
     })
   }
 
-  textAreaClick (data) {
+  textAreaClick () {
     this.ul.addEventListener('input', event => {
       if (event.target.className === 'textArea') {
         this.textAreaInput = event.target.value
-        // console.log(this.textAreaInput)
+        console.log(this.textAreaInput)
       }
     })
     this.ul.addEventListener('focusout', event => {
-      data = JSON.parse(localStorage.getItem('todos')) || []
+      let data = JSON.parse(localStorage.getItem('todos')) || []
       if (this.textAreaInput) {
         let parentId = parseInt(event.target.parentElement.id)
-        data = data.map(todo => (parentId === todo.id) ? { id: todo.id,
-          text: this.textAreaInput,
-          complete: todo.complete,
-          note: todo.note,
-          noteText: todo.noteText } : todo)
-        //  console.log(data)
+        data = data.map(todo =>
+          parentId === todo.id
+            ? {
+              id: todo.id,
+              text: this.textAreaInput,
+              complete: todo.complete,
+              note: todo.note,
+              noteText: todo.noteText
+            }
+            : todo
+        )
+        console.log(data)
         this.displayTodos(data)
         localStorage.setItem('todos', JSON.stringify(data))
         this.textAreaInput = ''
@@ -173,29 +179,26 @@ class View {
     })
   }
 
-  addNote (data) {
+  addNote () {
     this.noteBtn.addEventListener('click', event => {
-      data = JSON.parse(localStorage.getItem('todos')) || []
+      let data = JSON.parse(localStorage.getItem('todos')) || []
       let parentId = parseInt(event.target.parentElement.id)
-      // console.log(data)
+      console.log(data)
       for (let i = 0; i < data.length; i++) {
         if (data[i].id === parentId) data[i].note = true
         else data[i].note = false
       }
-      // data = data.map(todo => (parentId === todo.id) ?  : todo)
-      // console.log(parentId)
       let oldNotes = document.querySelector('.popdiv')
       if (oldNotes) oldNotes.remove()
       data.map(todo => {
         if (todo.note) this.displayNote(todo.id, todo.noteText, data)
       })
-      // localStorage.setItem('todos', JSON.stringify(data))
-      // this.saveNote(parentId)
+      console.log(data)
     })
   }
 
   displayNote (id, note, data) {
-    // console.log(data)
+    console.log(data)
     this.noteDiv = document.createElement('div')
     this.noteDiv.setAttribute('class', 'popdiv')
     this.popUpBox = document.createElement('textarea')
@@ -211,28 +214,32 @@ class View {
     this.noteDiv.append(this.popUpBox, this.saveBtn, this.cancelBtn)
     this.app.append(this.noteDiv)
     this.saveNote(id, data)
+    this.cancelNote(id, data)
   }
 
   saveNote (id, data) {
     this.saveBtn.addEventListener('click', event => {
-      // console.log(this.popUpBox.value)
-      // data = JSON.parse(localStorage.getItem('todos')) || []
-      // console.log(data)
-      data = data.map(todo => (id === todo.id) ? { id: todo.id,
-        text: this.textAreaInput,
-        complete: todo.complete,
-        note: todo.note,
-        noteText: this.popUpBox.value } : todo)
+      console.log(this.popUpBox.value)
+      console.log(data)
+      data = data.map(todo =>
+        id === todo.id
+          ? {
+            id: todo.id,
+            text: todo.text,
+            complete: todo.complete,
+            note: todo.note,
+            noteText: this.popUpBox.value
+          }
+          : todo
+      )
       localStorage.setItem('todos', JSON.stringify(data))
       document.querySelector('.popdiv').style.display = 'none'
-      // console.log(data)
-      // if (this.popUpBox.value === '') alert('Enter Notes')
-      // else {
-      //   // console.log(event.target.parentElement.parentElement)
-      //   let parentId = parseInt(event.target.parentElement.id)
-      //   // console.log(parentId)
-      //   //  data = data.map(todo => (parentId === todo.id) ? { id: todo.id, text: this.textAreaInput, complete: todo.complete, note: '' } : todo)
-      // }
+    })
+  }
+
+  cancelNote (id, data) {
+    this.cancelBtn.addEventListener('click', event => {
+      document.querySelector('.popdiv').style.display = 'none'
     })
   }
 }
