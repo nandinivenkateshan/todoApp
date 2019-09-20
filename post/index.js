9f571fb9c9e8465c4a2b8d896cabc57e5f47cce7
nandinivenkateshan <nandinivenkatesan809@gmail.com>
2019-09-20 13:20:29 +0530

    backend todo app

---
+++ post/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const db = require('./query')
+
+const app = express()
+const port = 3000
+app.use(express.static('../public'))
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.get('/todos', db.getTodo)
+app.post('/todos/checkBox', db.checkBoxClick)
+app.post('/todos', db.createTodoList)
+app.post('/todos/deleteList', db.deleteList)
+app.post('/todos/updateText', db.updateText)
+app.post('/todos/updateNote', db.updateNote)
+app.post('/todos/updateDate', db.updateDate)
+app.post('/todos/updatePriority', db.updatePriority)
+
+app.listen(port, () => console.log(`App running on the port ${port}`))
